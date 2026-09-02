b1aaeb7106b7fe55168a906f36a368f828a00d82
Ramesh <ramesh@local>
2026-09-02 15:03:12 +0530

    fix(alembic): shorten revision ID to fit Postgres alembic_version column limit

--- server/alembic/versions/20260902_add_module_to_pipeline_stages.py
+++ server/alembic/versions/20260902_add_module_to_pipeline_stages.py
@@ -1,6 +1,6 @@
 """Add module column to pipeline_stages
 
-Revision ID: 20260902_add_module_to_pipeline_stages
+Revision ID: 20260902_add_stage_module
 Revises: 20260828_add_dependent_rules
 Create Date: 2026-09-02
 """
@@ -8,7 +8,7 @@ Create Date: 2026-09-02
 from alembic import op
 import sqlalchemy as sa
 
-revision = '20260902_add_module_to_pipeline_stages'
+revision = '20260902_add_stage_module'
 down_revision = '20260828_add_dependent_rules'
 branch_labels = None
 depends_on = None
